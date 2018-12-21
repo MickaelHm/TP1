9d35a4c70722ff9ab8efff299e58e61f79c14403
MickaelHm <harmeau.mickael@gmail.com>
2018-12-21 11:29:33 +0100

    correction

--- Program.cs
+++ Program.cs
@@ -10,38 +10,45 @@ namespace TP1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Veuillez entrer votre poids en chiffre");
-            int weight = int.Parse(Console.ReadLine());
+            Console.WriteLine("Veuillez entrer votre poids en chiffre"); // on affiche un message demandant le poids du user
+            double weight = double.Parse(Console.ReadLine()); // Déclaration de la variable "weight" et on y affecte la valeur poids du user converti en double
             Console.WriteLine("Veuillez entrer votre taille en metre");
             double height = double.Parse(Console.ReadLine());
-            double IMC = (weight / (Math.Pow(height, 2)));
-            Console.WriteLine("Votre IMC est de " + IMC);
-            if (IMC <= 16.5)
+            double imc = Math.Round(weight / (Math.Pow(height, 2)),2); // déclaration de la variable imc de type double et a quoi elle est égale
+            // Pour avoir un chiffre arrondi a seulement 2 décimal on utilise - Math.Round( calcule ),2 - ,2 correspond a l'arrondi
+            Console.WriteLine("Votre IMC est de " + imc); // On donne l'IMC au user
+            if (imc <= 16.5 && imc >= 10)
             {
                 Console.WriteLine("Votre IMC indique une Dénutrition");
             }
-            else if (IMC > 16.5 && IMC <= 25)
+            else if (imc > 16.5 && imc <= 25)
             {
                 Console.WriteLine("Votre IMC une Maigreur");
             }
-            else if (IMC > 25 && IMC <= 30)
+            else if (imc > 25 && imc <= 30)
             {
                 Console.WriteLine("Votre IMC indique une Corpulence normale");
             }
-            else if (IMC > 30 && IMC <= 35)
+            else if (imc > 30 && imc <= 35)
             {
                 Console.WriteLine("Votre IMC indique une Obésité modérée");
             }
-            else if (IMC > 35 && IMC <= 40)
+            else if (imc > 35 && imc <= 40)
             {
                 Console.WriteLine("Votre IMC indique une Obésité sévère");
             }
-            else if (IMC > 40)
+            else if (imc > 40 && imc <=100)
             {
                 Console.WriteLine("Votre IMC indique une Obésité morbide ou massive");
             }
-
-
+            else if (imc < 10 || imc > 100)
+            {
+                Console.WriteLine("Vous ne devriez pas être vivant");
+            }
+            else
+            {
+                Console.WriteLine("oops il doit y avoir une erreur");
+            }
         }
     }
 }
